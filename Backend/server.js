037fcd49b7e997ffb3ef4b5c2fb4d13bced1da6f
Adham Magdy <adhammagdy614@gmail.com>
2024-01-17 12:59:18 +0200

    added socket to server and modfied its structure

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
-require('./services/googleAuthService');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 const authRoutes = require('./routes/authRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 const ping = require('./routes/pingRoute');
+
+// Import socket.io functionality
+const { runSocket } = require('./sockets/socket');
 
 const app = express();
 
@@ -18,8 +22,24 @@ app.use(
   })
 );
 
+// Initialize Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Create the server and attach socket.io
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      `http://localhost:${process.env.FRONTEND_PORT}`,
+      'https://admin.socket.io',
+    ],
+    credentials: true,
+  },
+});
+
+// Import socket.js functionality and run it
+runSocket(io);
 
 // Use the auth routes
 app.use(authRoutes);
@@ -32,6 +52,6 @@ app.use(ping);
 
 // Start the server
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
