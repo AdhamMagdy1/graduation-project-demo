dafc02ad68c2fd0632d117dbcffcc6b2e1fd90ad
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-03-15 21:19:48 +0200

    add restauraent socket and controllers

--- Backend/sockets/socket.js
+++ Backend/sockets/socket.js
@@ -1,20 +1,51 @@
 // socket.js
 const { getModelRes } = require('../middleware/conToModel');
-const runSocket = (io) => {
-  io.on('connection', (socket) => {
-    console.log(`user ${socket.id} connected`);
+const { instrument } = require("@socket.io/admin-ui");
+
+const runSocket = (io, mainNamespace, restaurantNamespace) => {
+  mainNamespace.on('connection', (socket) => {
+    console.log(`user ${socket.id} connected in chat namespace`);
 
     socket.on('message', (msg) => {
       const communicatedMassage = { socketId: socket.id, message: msg };
       // Uncomment the following line if you plan to use connectToModelFunction
       const emitMessage = getModelRes(communicatedMassage);
-      io.to(socket.id).emit('message', emitMessage);
+      mainNamespace.to(socket.id).emit('message', emitMessage);
     });
 
     socket.on('disconnect', () => {
-      console.log(`user ${socket.id} disconnected`);
+      console.log(`user ${socket.id} disconnected in chat namespace`);
     });
   });
+
+  instrument(io, {
+    auth: false,
+    mode: "development",
+  });
+
+  const order = {
+    orderId: 123,
+    orderName: "Pizza",
+    orderQuantity: 2,
+    state: "recieved"
+  };
+  restaurantNamespace.on('connection', (socket) => {
+    console.log(`restaurant ${socket.id} connected in restaurant namespace`);
+    // database order
+    // const order = getOrder();
+    // emit to restaurent room in restaurent namespace
+    // `Order: ${order.orderId} name: ${order.orderName} quantity: ${order.orderQuantity} state: ${order.state}`
+    socket.emit("order", order);
+    socket.on('changeState', (order) => {
+      order.state = "finished";
+      // sendToDatabase(order);
+      socket.emit("order", order);
+    });
+    socket.on('disconnect', () => {
+      console.log(`restaurant ${socket.id} disconnected in restaurant namespace`);
+    });
+  })
+
 };
 
 module.exports = { runSocket };
