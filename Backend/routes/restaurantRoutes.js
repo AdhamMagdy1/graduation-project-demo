49d8ee90b57efae8fa17afed373a343b9a1d6279
Adham Magdy <adhammagdy614@gmail.com>
2024-03-18 21:55:56 +0200

    modfied some controllers and add all the current resturant API documntations

--- Backend/routes/restaurantRoutes.js
+++ Backend/routes/restaurantRoutes.js
@@ -33,39 +33,40 @@ const {
   deleteMenu,
 } = require('../controllers/restaurantController');
 const authenticateUser = require('../middleware/authenticateUser');
+
+
 // Route to create a new restaurant Owner
 router.post('/create', createOwner);
-// Route to create a new restaurant Owner
 router.post('/login', loginOwner);
-router.get('/owner/:ownerId', getOwnerById); // working
-router.put('/owner/:ownerId', editOwner); // working
-router.delete('/owner/:ownerId', deleteOwner); // final one will not check it until create another owner
+router.get('/owner/',authenticateUser, getOwnerById); // working
+router.put('/owner/',authenticateUser, editOwner); // working
+router.delete('/owner/',authenticateUser, deleteOwner); // final one will not check it until create another owner
 
 // Apply authentication middleware to resturant info routes
 router.post('/setup', authenticateUser, createRestaurant);
-router.get('/all', getAllRestaurants);
+router.get('/all',authenticateUser, getAllRestaurants);
 router.get('/:restaurantId', authenticateUser, getRestaurantById);
 router.put('/:restaurantId', authenticateUser,  editRestaurantById);
 router.delete('/:restaurantId', authenticateUser, deleteRestaurantById);
 
 // products
 router.post('/products', authenticateUser, createProducts);
-router.post('/extras', authenticateUser, createExtra);
-router.post('/products/:ProductProductId/extras', authenticateUser, associateExtrasWithProduct);
-router.get('/products/productExtras', authenticateUser, getAllProductExtras);
 router.get('/products/all', authenticateUser, getAllProducts);
 router.get('/products/:productId', authenticateUser, getProductById);
 router.put('/products/:productId', authenticateUser, editProductById);
 router.delete('/products/:productId', authenticateUser, deleteProductById);
 
 // extras
-router.get('/extras/all', getAllExtras);
-router.get('/extras/:extraId', getExtraById);
-router.put('/extras/:extraId', editExtraById);
-router.delete('/extras/:extraId', deleteExtraById);
+router.post('/extras', authenticateUser, createExtra);
+router.get('/extras/all',authenticateUser, getAllExtras);
+router.get('/extras/:extraId',authenticateUser, getExtraById);
+router.put('/extras/:extraId',authenticateUser, editExtraById);
+router.delete('/extras/:extraId',authenticateUser, deleteExtraById);
 
-
+//productsExstras 
+router.post('/products/:ProductProductId/extras', authenticateUser, associateExtrasWithProduct);
 router.get('/products/:ProductProductId/extras', authenticateUser, getAssociatedExtrasForProduct);
+router.get('/products/productExtras', authenticateUser, getAllProductExtras);
 router.put('/products/:ProductProductId/extras/:ExtraExtraId', authenticateUser, editProductExtra);
 router.delete('/products/:ProductProductId/extras/', authenticateUser, deleteProductExtras);
 
@@ -82,4 +83,7 @@ router.post(
 router.get('/menu/:menuId', authenticateUser, getMenu);
 router.put('/menu/:menuId', upload.single('menuImage'), authenticateUser, editMenu);
 router.delete('/menu/:menuId', authenticateUser, deleteMenu);
+
+
+
 module.exports = router;
