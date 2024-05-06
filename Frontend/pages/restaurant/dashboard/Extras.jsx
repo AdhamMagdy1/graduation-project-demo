1b1009c95a4cab635031ea11612736c47518ddaf
Mayada <mayadaelsayed383@gmail.com>
2024-05-06 11:47:56 +0300

    create category, extras, & stats pages

---
+++ Frontend/pages/restaurant/dashboard/Extras.jsx
@@ -0,0 +1,12 @@
+import SideBar from './SideBar';
+
+const Extras = () => {
+	return (
+		<div className='page-container'>
+			<SideBar />
+			<h2>extras</h2>
+		</div>
+	);
+};
+
+export default Extras;
