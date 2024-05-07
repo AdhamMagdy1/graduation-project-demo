b946fc711961b508ddbc3ffcd552d93f12423798
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-05-07 19:04:48 +0300

    returned postgres database connection config to cloud instead of local

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,25 +1,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const sequelize = new Sequelize(
-  // process.env.DATABASE_NAME,
-  // process.env.DATABASE_USERNAME,
-  // process.env.DATABASE_PASSWORD,
-  // {
-  //   host: process.env.DATABASE_HOST,
-  //   dialect: 'postgres',
-  //   dialectOptions: {
-  //     ssl: {
-  //       require: 'true',
-  //     },
-  //   },
-  // }
-  process.env.LOCAL_DATABASE_NAME,
-  process.env.LOCAL_DATABASE_USERNAME,
-  process.env.LOCAL_DATABASE_PASSWORD,
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
   {
-    host: process.env.LOCAL_DATABASE_HOST,
+    host: process.env.DATABASE_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: 'true',
+      },
+    },
   }
+  // process.env.LOCAL_DATABASE_NAME,
+  // process.env.LOCAL_DATABASE_USERNAME,
+  // process.env.LOCAL_DATABASE_PASSWORD,
+  // {
+  //   host: process.env.LOCAL_DATABASE_HOST,
+  //   dialect: 'postgres',
+  // }
 );
 
 const testDbConnection = async () => {
@@ -28,7 +28,7 @@ const testDbConnection = async () => {
     console.log('Connection has been established successfully.');
     // add to the sync() { force: true } or { alter: true } if you have pre data in you database. Note: { force: true } will delete all you data!! but it also will allow sequlize to work properly.
     sequelize
-      .sync({ force: true })
+      .sync()
       .then(() => console.log('Models synced successfully!'))
       .catch((error) => console.error('Error syncing models:', error));
   } catch (error) {
