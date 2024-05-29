34e318448e61629ae062cbd533127cc2ea967111
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-05-30 01:18:39 +0300

    remove: redis related controllers

--- Backend/routes/customerRoutes.js
+++ Backend/routes/customerRoutes.js
@@ -5,15 +5,11 @@ const customerController = require('../controllers/customerController');
 const authenticateUser = require('../middleware/authenticateUser');
 
 
-
 router.post('/login', customerController.login);
-router.post('/watingOrder', authenticateUser, customerController.createWaitingOrder);
-router.post('/createOrder', authenticateUser, customerController.createOrder);
 router.post('/createAddress', authenticateUser, customerController.createAddress);
-router.post('/createPhoneNumber', authenticateUser, customerController.createCustomerPhoneNumber);
 router.get('/all', customerController.getAllCustomers);
 router.get('/allOrders', customerController.getAllOrders);
-router.put('/editOrderStatus', authenticateUser, customerController.editOrderStatus);
-
+router.get('/allPhoneNumbers',  customerController.getAllCustomersPhoneNumber);
+router.delete('/order/all', customerController.deleteAllOrders);
 
 module.exports = router;
