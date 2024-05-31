33ed3b0257be290f4ddfd768799aece667e52519
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-05-31 17:03:32 +0300

    imported Customer model for the edit in authenticate middleware

--- Backend/routes/customerRoutes.js
+++ Backend/routes/customerRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middleware/authMiddleware');
 const customerController = require('../controllers/customerController');
-const authenticateUser = require('../middleware/authenticateUser');
-
+const { authenticateUser } = require('../middleware/authenticateUser');
+const { Customer } = require("../models/allModels");
 
 router.post('/login', customerController.login);
-router.post('/createAddress', authenticateUser, customerController.createAddress);
+router.post('/createAddress', authenticateUser(Customer), customerController.createAddress);
 router.get('/all', customerController.getAllCustomers);
 router.get('/allOrders', customerController.getAllOrders);
 router.get('/allPhoneNumbers',  customerController.getAllCustomersPhoneNumber);
