4ff08ca2fec61103e528644eebed0dc43f54ecb1
Mayada <mayadaelsayed383@gmail.com>
2024-06-04 16:44:31 +0300

    create upload menu form and send request

--- Frontend/pages/restaurant/new/Menus.jsx
+++ Frontend/pages/restaurant/new/Menus.jsx
@@ -1,10 +1,92 @@
+import { useState } from "react";
+import Loading from '../../../src/Loading';
+import { useNavigate } from 'react-router-dom';
+
 
 const Menus = () => {
+	const URL = import.meta.env.VITE_REACT_API_URL;
+	const navigate = useNavigate();
+
+	const [menu, setMenu] = useState(null);
+	const [description, setDescription] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const formData = new FormData();
+		formData.append('description', description);
+		if (menu) {
+			formData.append('menuImage', menu);
+		}
+
+		setIsLoading(true);
+		try {
+			const token = localStorage.getItem('token');
+			const response = await fetch(`${URL}/restaurant/menu/upload`, {
+				method: 'POST',
+				headers: {
+					Authorization: token,
+				},
+				body: formData,
+			});
+			if (!response.ok) {
+				setIsLoading(false);
+				const result = await response.json();
+				// setErrMsg(result.message);
+				console.log(result.message);
+			} else {
+				console.log('Menu uploaded successfully');
+				// resetRestaurant();
+				navigate(`/restaurant/menu`);
+
+			}
+
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	if (isLoading) {
+		return <div className="page-container">
+			<Loading />
+		</div>;
+	}
+
 	return (
-		<form>
-			<input type='file' />
-			<button type='button'>done</button>
-		</form>
+		<div className="container">
+			<h2 className="container-heading">welcome to <div className="logo" style={{ display: 'inline', color: 'black' }}>chef<span className="second-word">bot</span></div></h2>
+			<p>please upload your menu here:</p>
+			<form>
+				{/* menu files */}
+				<label htmlFor='menuImage'>Menu</label>
+				<input
+					className='input input-block'
+					type="file"
+					id='menuImage'
+					name="menuImage"
+					onChange={(event) => {
+						setMenu(event.target.files[0]);
+						console.log(event.target.files);
+					}}
+					required
+				/>
+
+				{/* description */}
+				<label htmlFor="description">description</label>
+				<input
+					type="text"
+					className='input input-block '
+					name='description'
+					id='description'
+					placeholder='description'
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+				/>
+
+				<button className='btn btn-block' onClick={handleSubmit} type='submit'>done</button>
+			</form>
+		</div>
 	);
 };
 
