bdbbff5ecf82e04330b1b63ac27027fd586d6dfe
Mayada <mayadaelsayed383@gmail.com>
2024-06-05 23:59:44 +0300

    delete functionality

--- Frontend/pages/restaurant/dashboard/Menu.jsx
+++ Frontend/pages/restaurant/dashboard/Menu.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useGlobalContext } from './context';
 import { FaTimes } from 'react-icons/fa';
+import useDeleteItem from '../../../src/hooks/useDeleteItem';
 
 const Menu = () => {
 
@@ -144,6 +145,20 @@ const Menu = () => {
 		}
 	};
 
+	//delete menu item: 
+	const { deleteItem } = useDeleteItem(`/restaurant/menu/`);
+	const deleteMenu = async (id) => {
+		setIsLoading(true);
+		const resp = await deleteItem(id);
+		if (resp) {
+			setIsLoading(false);
+			setTrigger(prev => prev + 1);
+		} else {
+			console.log('Sorry, something went wrong. cannot delete item');
+			setIsLoading(false);
+		}
+	};
+
 	if (isLoading) {
 		return (<div className="page-container">
 			<SideBar />
@@ -171,7 +186,7 @@ const Menu = () => {
 								<div className="menu-description">
 									<p>{image.description}</p>
 									<div className="actions">
-										<button className="btn-action btn-red">
+										<button onClick={() => deleteMenu(image.menuId)} className="btn-action btn-red">
 											<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M7 3h2a1 1 0 0 0-2 0M6 3a2 2 0 1 1 4 0h4a.5.5 0 0 1 0 1h-.564l-1.205 8.838A2.5 2.5 0 0 1 9.754 15H6.246a2.5 2.5 0 0 1-2.477-2.162L2.564 4H2a.5.5 0 0 1 0-1zm1 3.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0zM9.5 6a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0v-5a.5.5 0 0 1 .5-.5m-4.74 6.703A1.5 1.5 0 0 0 6.246 14h3.508a1.5 1.5 0 0 0 1.487-1.297L12.427 4H3.573z" /></svg>
 										</button>
 										<button onClick={() => handleEdit(image.menuId, image.description)} className="btn-action btn-green" >
