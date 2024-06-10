4d546e67a5ca23824f707e7dfa119e9ad41f0f00
Mayada <mayadaelsayed383@gmail.com>
2024-06-10 14:20:27 +0300

    enable copying restaurant link

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AppProvider } from '../pages/restaurant/dashboard/context.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { AppProvider } from '../pages/restaurant/dashboard/context.jsx';
+import "react-toastify/dist/ReactToastify.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </AppProvider>
   </React.StrictMode>,
-)
+);
