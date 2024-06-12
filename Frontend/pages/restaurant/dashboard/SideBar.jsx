77827c78b61570f2b91c7357b4f698ada1374042
Mayada <mayadaelsayed383@gmail.com>
2024-06-12 13:55:07 +0300

    add log out functionality

--- Frontend/pages/restaurant/dashboard/SideBar.jsx
+++ Frontend/pages/restaurant/dashboard/SideBar.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SideBar = () => {
 
+	const navigate = useNavigate();
+
 	const [isAddsOpen, setIsAddsOpen] = useState(false);
 	const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+	const logout = () => {
+		localStorage.removeItem('token');
+		navigate('/restaurant/login');
+	};
 
 
 	return (
 		<>
 			<section className="side-bar">
-				<div className="side-container">
+				<div style={{ position: 'relative' }} className="side-container">
 					<div className="logo">chef<span className="second-word">bot</span></div>
 					<div className="side-links">
+
+						<Link to="/restaurant/orders" className="side-link" >
+							<div className="link-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048"><path fill="#eaa90f" d="m2029 1453l-557 558l-269-270l90-90l179 178l467-466zM1024 640H640V512h384zm0 256H640V768h384zm-384 128h384v128H640zM512 640H384V512h128zm0 256H384V768h128zm-128 128h128v128H384zm768-384V128H256v1792h896v128H128V0h1115l549 549v731l-128 128V640zm128-128h293l-293-293z" /></svg>
+							</div>
+							<div className="a">orders</div>
+						</Link>
 
 						<Link to="/restaurant/stats" className="side-link" >
 							<div className="link-icon">
@@ -21,12 +35,6 @@ const SideBar = () => {
 							<div className="a">stats</div>
 						</Link>
 
-						<Link to="/restaurant/orders" className="side-link" >
-							<div className="link-icon">
-								<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048"><path fill="#eaa90f" d="m2029 1453l-557 558l-269-270l90-90l179 178l467-466zM1024 640H640V512h384zm0 256H640V768h384zm-384 128h384v128H640zM512 640H384V512h128zm0 256H384V768h128zm-128 128h128v128H384zm768-384V128H256v1792h896v128H128V0h1115l549 549v731l-128 128V640zm128-128h293l-293-293z" /></svg>
-							</div>
-							<div className="a">orders</div>
-						</Link>
 
 						<div className="dropdown">
 							<button
@@ -93,6 +101,13 @@ const SideBar = () => {
 								)
 							}
 						</div>
+
+						<bottun onClick={() => logout()} style={{ position: 'absolute', bottom: '2rem' }} className='log-out'>
+							<div className="link-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="#c9900c" strokeLinecap="round" strokeWidth="1.5"><path strokeLinejoin="round" d="M15 12H2m0 0l3.5-3M2 12l3.5 3" /><path d="M9.002 7c.012-2.175.109-3.353.877-4.121C10.758 2 12.172 2 15 2h1c2.829 0 4.243 0 5.122.879C22 3.757 22 5.172 22 8v8c0 2.828 0 4.243-.878 5.121c-.769.769-1.947.865-4.122.877M9.002 17c.012 2.175.109 3.353.877 4.121c.641.642 1.568.815 3.121.862" /></g></svg>
+							</div>
+							<div className="a">log out</div>
+						</bottun>
 
 					</div>
 				</div>
