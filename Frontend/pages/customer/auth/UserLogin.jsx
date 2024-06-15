1564d12797a82e69f31dc40a38180339c1a4ec50
Adham Magdy <adhammagdy614@gmail.com>
2024-06-15 17:04:53 +0300

    fixed auth error

--- Frontend/pages/customer/auth/UserLogin.jsx
+++ Frontend/pages/customer/auth/UserLogin.jsx
@@ -1,64 +1,61 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
 
 function UsrLogin() {
   const [errMsg, setErrMsg] = useState('');
   const [success, setSuccess] = useState(false);
-  const navigate = useNavigate();
 
   useEffect(() => {
     setErrMsg('');
   }, [success]);
 
   const responseMessage = (response) => {
-    console.log(response);
-    fetch('http://localhost:5000/login', {
+    fetch('http://localhost:5000/customer/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${response.credential}`,
       },
-      body: JSON.stringify(response),
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
       .then((data) => {
         console.log(data);
-        window.localStorage.setItem('user', JSON.stringify(data.user));
-        window.localStorage.setItem('token', data.token);
-        setSuccess(true);
+        if (data) {
+          window.localStorage.setItem('user', JSON.stringify(data.customer));
+          window.localStorage.setItem('token', data.token);
+          setSuccess(true);
+          window.location.href = '/customer/chat';
+        } else {
+          setErrMsg('Login failed. Please try again.');
+        }
       })
       .catch((error) => {
-        console.log(error);
+        console.error('Error during login:', error);
         setErrMsg('Login failed. Please try again.');
       });
   };
 
   const errorMessage = (error) => {
-    console.log(error);
+    console.error('Google login error:', error);
     setErrMsg('Login failed. Please try again.');
   };
 
-  useEffect(() => {
-    if (success) {
-      navigate('/chat');
-    }
-  }, [success, navigate]);
-
   return (
     <>
-      {success ? (
-        <p>Redirecting...</p>
-      ) : (
-        <section className="login">
-          <p className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">
-            {errMsg}
-          </p>
-          <div className="container">
-            <h1>LOGIN</h1>
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-          </div>
-        </section>
-      )}
+      <section className="login">
+        <p className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">
+          {errMsg}
+        </p>
+        <div className="container">
+          <h1>LOGIN</h1>
+          <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        </div>
+      </section>
     </>
   );
 }
