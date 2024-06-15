b86c7994340ee3974995d054e1d02b5eddb3b3d7
Adham Magdy <adhammagdy614@gmail.com>
2024-06-15 18:39:56 +0300

    added sentmint visualzation in the state page and connected it with the backend

--- Frontend/pages/restaurant/dashboard/Stats.jsx
+++ Frontend/pages/restaurant/dashboard/Stats.jsx
@@ -1,12 +1,19 @@
 import SideBar from './SideBar';
+import LineChartComponent from './charts/LineChartComponent';
+import PieChartComponent from './charts/PieChartComponent';
 const Stats = () => {
-	return (
-
-		<div className='page-container'>
-			<SideBar />
-			<h2>Stats</h2>
-		</div>
-	);
+  return (
+    <div className="page-container">
+      <SideBar />
+      <div className="AppChart">
+        <h1>Sentiment Analysis Visualizations</h1>
+        <div className="charts">
+          <LineChartComponent />
+          <PieChartComponent />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
