d49dbb86d6961f0d16fc71319e98f543beb3316a
Mayada <mayadaelsayed383@gmail.com>
2024-06-17 11:52:20 +0300

    add chat style to index style sheet

--- Frontend/pages/customer/Chat.jsx
+++ Frontend/pages/customer/Chat.jsx
@@ -1,16 +1,94 @@
 /* eslint-disable react/prop-types */
 import { nanoid } from 'nanoid';
-import './chat.css';
 import { useState, useEffect, useRef } from 'react';
 import logo from '../images/logo.png';
 import exampleImage from '../images/example-1.jpeg';
 import io from 'socket.io-client';
-
+import { useGlobalContext } from '../restaurant/dashboard/context';
+import extractedData from '../restaurant/new/extractedData.json';
 
 
 const socket = io('http://localhost:5000/chat');
 
 const Chat = () => {
+
+  const {
+    isFirstModalOpen,
+    openFirstModal,
+    closeFirstModal,
+    isSecondModalOpen,
+    openSecondModal,
+    closeSecondModal
+  } = useGlobalContext();
+
+  //address variables:
+  const URL = import.meta.env.VITE_REACT_API_URL;
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzE4MzY5MTQzLCJleHAiOjE3MTkwMTcxNDN9.7nSK6tG0R2jnBE91aC9aTIVRAkTSR-9pp-G4ISHJK5s';
+  const [isloading, setIsloading] = useState(false);
+
+  const cities = extractedData;
+  const [selectedCity, setSelectedCity] = useState('');
+  const [areas, setAraes] = useState([]);
+  const [selectedArea, setSelectedArea] = useState('');
+
+  const [street, setStreet] = useState('');
+  const [building, setBuilding] = useState('');
+  const [apartment, setApartment] = useState('');
+
+  const changeCity = (e) => {
+    setSelectedCity(e.target.value);
+    setAraes(cities.find((city) => city.code === e.target.value).cityDataModels);
+  };
+
+  const changeArea = (e) => {
+    setSelectedArea(e.target.value);
+  };
+
+  const handleSubmitAddress = async (e) => {
+    e.preventDefault();
+    const body = { addressDescription: `${building} ${street} str, apartment ${apartment}, ${selectedArea}, ${selectedCity} ` };
+    // console.log(body);
+    setIsloading(true);
+    try {
+      const resp = await fetch(`${URL}/customer/createAddress`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+        body: JSON.stringify(body),
+      });
+      if (resp.ok) {
+        setIsloading(false);
+        const customerData = await resp.json();
+        localStorage.setItem('addressId', customerData.address.addressId);
+        localStorage.setItem('customerId', customerData.address.customerId);
+        // navigate(`/customer/chat`);
+        closeSecondModal();
+      } else {
+        setIsloading(false);
+        console.log(resp.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmitLogin = (e) => {
+    e.preventDefault();
+    setIsloading(true);
+    setTimeout(() => {
+      setIsloading(false);
+      openSecondModal();
+      closeFirstModal();
+    }, 3000);
+
+  };
+
+  useEffect(() => {
+    openFirstModal();
+  }, []);
+
   const chatRef = useRef();
 
   const [messages, setMessages] = useState([]);
@@ -57,6 +135,11 @@ const Chat = () => {
     }
   };
 
+  //accessing the restaurantId:
+  // const restaurantId = (new URLSearchParams(window.location.search)).get("restaurantId");
+  // console.log(restaurantId);
+
+
   return (
     <>
       <section className='chat-section'>
@@ -101,6 +184,100 @@ const Chat = () => {
           </div>
         </div>
       </section>
+
+      <div className={isFirstModalOpen ? "modal-overlay show-modal" : "modal-overlay"} >
+        <div className="form-holder">
+          {
+            isloading &&
+            <div style={{ height: '100%' }} className="overlay"></div>
+          }
+          <form style={{ height: 'fit-content' }} onSubmit={handleSubmitLogin} className='address-form'>
+            <button className='address-btn' type='submit' >login with google acount</button>
+          </form>
+        </div>
+      </div>
+
+      <div className={isSecondModalOpen ? "modal-overlay show-modal" : "modal-overlay"}>
+        <div className="form-holder">
+          {
+            isloading &&
+            <div className="overlay"></div>
+          }
+          <form onSubmit={handleSubmitAddress} className='address-form'>
+            {/* city */}
+            <div className="input-holder">
+              <label>city <span style={{ color: 'red' }} >*</span></label>
+              <select required className='input-block' value={selectedCity} onChange={changeCity} >
+                <option hidden>Select a city</option>
+                {
+                  cities.map((city) => {
+                    return <option value={city.code} key={nanoid()}>{city.code}</option>;
+
+                  })
+                }
+              </select>
+            </div>
+
+            {/* Area */}
+            <div className="input-holder">
+              <label>area <span style={{ color: 'red' }} >*</span> </label>
+              <select required className='input-block' value={selectedArea} onChange={changeArea}>
+                <option hidden>Select an area</option>
+                {
+                  areas.map((area) => {
+                    return <option value={area.namePrimaryLang} key={area.id}>
+                      {area.namePrimaryLang}
+                    </option>;
+                  })
+                }
+              </select>
+            </div>
+
+            {/* street */}
+            <div className="input-holder">
+              <label>street name <span style={{ color: 'red' }} >*</span></label>
+              <input
+                style={{ caretColor: '#f49191' }}
+                className='input-block'
+                type="text"
+                placeholder='Street name'
+                value={street}
+                onChange={(e) => setStreet(e.target.value)}
+                required
+              />
+            </div>
+
+            {/* buliding */}
+            <div className="input-holder">
+              <label>buliding name/number</label>
+              <input
+                style={{ caretColor: '#f49191' }}
+                className='input-block'
+                type="text"
+                placeholder='Builing name/number'
+                value={building}
+                onChange={(e) => setBuilding(e.target.value)}
+              />
+            </div>
+
+            {/* apartment */}
+            <div className="input-holder">
+              <label>apartment number</label>
+              <input
+                style={{ caretColor: '#f49191' }}
+                className='input-block'
+                type="text"
+                placeholder='Apartment number'
+                value={apartment}
+                onChange={(e) => setApartment(e.target.value)}
+              />
+            </div>
+
+            <button className='address-btn' type='submit' >done</button>
+          </form>
+        </div>
+      </div>
+
     </>
   );
 };
